ecf48b1f8b246edc03762965ecc72f0bf55331dc
Sahadat Hosen <ssar96540@gmail.com>
2024-09-18 20:59:36 +0600

    Added Primary and Secondary color in tailwind config & modify feedback Componenet

---
+++ src/components/home/Feedback.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Feedback : React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Feedback ;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className='text-4xl font-bold'>
+        <div className='text-4xl text-primary font-bold'>
             Welcome 🌱🎉
         </div>
     );
