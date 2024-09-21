7660db317374cebe7443b57682e30cd2695949f5
hammadsadi <hammad.sadi@yahoo.com>
2024-09-21 23:08:19 +0600

    Added Comment

--- src/components/shared/SectionHeading.tsx
+++ src/components/shared/SectionHeading.tsx
@@ -3,6 +3,7 @@ import { sectionHeadingProps } from "../../types/Types";
 const SectionHeading = ({ title, subTitle }: sectionHeadingProps) => {
   return (
     <div className="text-center mb-6 md:mb-10">
+      {/* This is Sub Heading */}
       <h5 className="text-primary text-center  font-light my-3 font-great-vibes text-3xl">{subTitle}</h5>
       <h2 className="font-medium text-3xl text-center uppercase text-[#333333]">{title}</h2>
     </div>
