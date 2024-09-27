ca1be5673a8ccacb802243571a859c3ad288205a
devmahin <mahinhowlader123@gmail.com>
2024-09-27 11:53:27 +0530

    all code pull

--- src/components/cart/PriceDetails.tsx
+++ src/components/cart/PriceDetails.tsx
@@ -13,34 +13,36 @@ const PriceDetails = () => {
   const totalDiscount = cart.reduce((accumulator: number, currentValue: any) => {
     return accumulator + currentValue?.discount;
   }, 0);
+
   return (
-    <div className="mt-8 ">
-       {/* Price Info */}
-       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-      <PriceDetailsIteItem title="Discount" price={totalDiscount}/>
-      <PriceDetailsIteItem title="Delivery" price={966}/>
-      <PriceDetailsIteItem title="Subtotal" price={966}/>
-      <PriceDetailsIteItem title="Total" price={Math.ceil(totalPrice)}/>
+    <div className="mt-8">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        <PriceDetailsIteItem title="Discount" price={totalDiscount} />
+        <PriceDetailsIteItem title="Delivery" price={966} />
+        <PriceDetailsIteItem title="Subtotal" price={966} />
+        <PriceDetailsIteItem title="Total" price={Math.ceil(totalPrice)} />
+      </div>
       
-       </div>
-       {/* Cupon Code */}
-       <div className="max-w-3xl mx-auto">
-        <p className="my-4">If You Have Promotion Code, Please Inter it here</p>
+      {/* Coupon Code */}
+      <div className="max-w-3xl mx-auto">
+        <p className="my-4">If You Have Promotion Code, Please Enter it here</p>
         <div className="grid md:grid-cols-3 grid-cols-1">
-           <div className="flex gap-[1px] items-center md:col-span-2">
-           <input type="text" className="border w-full p-2  outline-none focus:border-primary rounded"/>  <button className="btn-primary py-3 px-6">Apply</button>
-           </div>
+          <div className="flex gap-[1px] items-center md:col-span-2">
+            <input
+              type="text"
+              className="border w-full p-2 outline-none focus:border-primary rounded"
+            />
+            <button className="btn-primary py-3 px-6">Apply</button>
+          </div>
           <div className="flex gap-[2px] items-center justify-end">
-          <button className="btn-primary py-3 px-6">checkout</button>
-          <button className="btn-primary py-[13px] px-3 "><TbShoppingBag className="text-lg"/></button>
-       
+            <button className="btn-primary py-3 px-6">Checkout</button>
+            <button className="btn-primary py-[13px] px-3">
+              <TbShoppingBag className="text-lg" />
+            </button>
           </div>
         </div>
       </div>
-    );
-  }
-
-  return <UserInfoForm user={user} />;
+    </div>
+  );
 };
-
 export default PriceDetails;
