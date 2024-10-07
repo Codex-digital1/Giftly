84e3d0aaaeede3b694addceac83669e499d6199f
Sahadat Hosen <ssar96540@gmail.com>
2024-10-07 19:50:48 +0600

    Delete functionality implemented for DeleteAGift

--- src/components/Dashboard/Gift/GiftListItem/GiftListItem.tsx
+++ src/components/Dashboard/Gift/GiftListItem/GiftListItem.tsx
@@ -3,12 +3,25 @@ import TableTd from "../../../shared/TableTd";
 import { FaTrash } from "react-icons/fa";
 import { FaRegPenToSquare } from "react-icons/fa6";
 import useAuth from "../../../../Provider/useAuth";
+import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
 
 const GiftListItem = ({setUpdateGiftAddModal}: {setUpdateGiftAddModal: (value:boolean)=> void}) => {
-
+const axiosPublic = useAxiosPublic();
 const { gifts, loading } = useAuth()?? {};
 console.log(gifts);
+ 
+  // Handle delete click
+  const handleDelete = async(id: string) => {
+    try {
+      // Perform the delete request
+       const response = await axiosPublic.delete(`/${id}`)
+       console.log(response);
 
+
+    } catch (error) {
+      console.error("Error deleting gift:", error);
+    }
+  };
   return (
    <>
  {gifts?.map((gift, index) => (
@@ -36,7 +49,13 @@ console.log(gifts);
 
       <td className="whitespace-nowrap px-4 py-2 text-base font-medium text-gray-800">
         <div className="flex gap-1 justify-center">
-         <button className="btn-primary"><FaTrash/></button>
+        <button 
+  onClick={() => handleDelete(gift._id)} // Pass the id correctly
+  className="btn-primary"
+>
+  <FaTrash />
+</button>
+
          <button onClick={()=> setUpdateGiftAddModal(true)}  className="text-white btn-primary bg-[#2ed573] hover:bg-[#23a358]"><FaRegPenToSquare/></button>
       
         </div>
