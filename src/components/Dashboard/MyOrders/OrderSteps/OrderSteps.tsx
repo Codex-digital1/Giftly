64ef228bdf532b4c6fdb3956eebc63bc18910db1
hammadsadi <hammad.sadi@yahoo.com>
2024-10-09 15:11:35 +0600

    Updated Color

--- src/components/Dashboard/MyOrders/OrderSteps/OrderSteps.tsx
+++ src/components/Dashboard/MyOrders/OrderSteps/OrderSteps.tsx
@@ -1,7 +1,6 @@
 import "./StepBar.css";
 import useGetSpecificOrders from "../../../../Hooks/useGetSpecificOrders";
 import { FaCheck } from "react-icons/fa";
-
 import { OrderTypes } from "../../../../types/Types";
 import { useParams } from "react-router-dom";
 
@@ -9,36 +8,34 @@ const StepBar = () => {
   const [data] = useGetSpecificOrders();
   const { id } = useParams();
   const trackOrder = data?.find((item: OrderTypes) => item._id === id);
-  const steps: string[] = ["Pending", "Processing", "Shipping", "Delivered"];
+  const steps = ["Pending", "Processing", "Shipping", "Delivered"];
 
   // Check Active or False
   const getStepStatus = (step: string): boolean => {
-    // Validation
     if (!trackOrder) return false;
-    // Get Index Number
     const orderStatusIndex = steps.indexOf(trackOrder.order_status);
     const stepIndex = steps.indexOf(step);
-
-    // Return True or false
     return stepIndex <= orderStatusIndex;
   };
 
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="flex flex-col min-h-[50%] md:h-fit md:flex-row justify-between">
-        {steps.map((item, idx) => (
-          <div
-            key={idx}
-            className={`step-item ${
-              getStepStatus(item) ? "active completed" : ""
-            } ${trackOrder?.order_status === item ? "completed" : ""}`}
-          >
-            <div className="step">
-              {getStepStatus(item) ? <FaCheck /> : idx + 1}
+        {steps.map((item, idx) => {
+          return (
+            <div
+              key={idx}
+              className={`step-item ${
+                getStepStatus(item) ? "active completed" : ""
+              } ${trackOrder?.order_status === item ? "completed" : ""}`}
+            >
+              <div className="step">
+                {getStepStatus(item) ? <FaCheck /> : idx + 1}
+              </div>
+              <p>{item}</p>
             </div>
-            <p>{item}</p>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
