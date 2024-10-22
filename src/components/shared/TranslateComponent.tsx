34f4f73b33e34d9896d6845033a488fa33480067
Sahadat Hosen <ssar96540@gmail.com>
2024-10-22 18:35:17 +0600

    Solved some typescript error inside the TranslateComponenet

--- src/components/shared/TranslateComponent.tsx
+++ src/components/shared/TranslateComponent.tsx
@@ -1,19 +1,85 @@
-import { useEffect, useRef, useState } from "react";
+// import { useEffect, useRef, useState } from "react";
+
+// const TranslateComponent: React.FC = () => {
+//     const googleTranslateRef = useRef<HTMLDivElement | null>(null);
+//     const [change, setChange] = useState<string>("en");
+
+//     useEffect(() => {
+//         const handleLanguageChange = () => {
+//             const googleTranslateElement = document.querySelector<HTMLSelectElement>(".goog-te-combo");
+//             if (googleTranslateElement) {
+//                 googleTranslateElement.value = change;
+//                 googleTranslateElement.dispatchEvent(new Event("change"));
+//             }
+//         };
+//         handleLanguageChange();
+//     }, [change]);
+
+//     useEffect(() => {
+//         const googleTranslateElementInit = () => {
+//             if (window.google && window.google.translate && window.google.translate.TranslateElement) {
+//                 new window.google.translate.TranslateElement(
+//                     {
+//                         pageLanguage: "en",
+//                         includedLanguages: "en,bn,es,fr,de,hi,it,ja,ko,zh-CN,ru",
+//                         layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+//                     },
+//                     googleTranslateRef.current
+//                 );
+
+//                 const intervalId = setInterval(() => {
+//                     const imgElement = document.querySelector<HTMLImageElement>('.goog-te-gadget-simple > img');
+//                     if (imgElement) {
+//                         // Create a new image
+//                         const newImage = document.createElement('img');
+//                         newImage.src = '/language.png'; 
+//                         // newImage.style.height = '20px'; 
+//                         // newImage.style.width = '20px'; 
+//                         newImage.alt = 'Language Icon'; 
+//                         newImage.className = 'iconColor';
+
+                        
+//                         imgElement.parentNode?.replaceChild(newImage, imgElement);
+//                         clearInterval(intervalId); 
+//                     }
+//                 }, 100);
+
+//                 return () => clearInterval(intervalId);
+//             }
+//         };
+
+//         const script = document.createElement("script");
+//         script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+//         window.googleTranslateElementInit = googleTranslateElementInit;
+//         document.body.appendChild(script);
+
+//         return () => {
+//             document.body.removeChild(script);
+//         };
+//     }, []);
+
+//     return (
+//         <div ref={googleTranslateRef} className="flex justify-center items-end ">
+//             {/* Google Translate will render here */}
+//         </div>
+//     );
+// };
+// export default TranslateComponent;
+
+
+
+import { useEffect, useRef } from "react";
+
+// Extending the Window interface to include the google property
+declare global {
+    interface Window {
+        google: any;
+        googleTranslateElementInit: () => void;
+    }
+}
 
 const TranslateComponent: React.FC = () => {
     const googleTranslateRef = useRef<HTMLDivElement | null>(null);
-    const [change, setChange] = useState<string>("en");
-
-    useEffect(() => {
-        const handleLanguageChange = () => {
-            const googleTranslateElement = document.querySelector<HTMLSelectElement>(".goog-te-combo");
-            if (googleTranslateElement) {
-                googleTranslateElement.value = change;
-                googleTranslateElement.dispatchEvent(new Event("change"));
-            }
-        };
-        handleLanguageChange();
-    }, [change]);
 
     useEffect(() => {
         const googleTranslateElementInit = () => {
@@ -32,15 +98,12 @@ const TranslateComponent: React.FC = () => {
                     if (imgElement) {
                         // Create a new image
                         const newImage = document.createElement('img');
-                        newImage.src = '/language.png'; 
-                        // newImage.style.height = '20px'; 
-                        // newImage.style.width = '20px'; 
-                        newImage.alt = 'Language Icon'; 
+                        newImage.src = '/language.png';
+                        newImage.alt = 'Language Icon';
                         newImage.className = 'iconColor';
 
-                        
                         imgElement.parentNode?.replaceChild(newImage, imgElement);
-                        clearInterval(intervalId); 
+                        clearInterval(intervalId); // Clear the interval after replacement
                     }
                 }, 100);
 
@@ -48,6 +111,7 @@ const TranslateComponent: React.FC = () => {
             }
         };
 
+        // Dynamically add the Google Translate script
         const script = document.createElement("script");
         script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
         window.googleTranslateElementInit = googleTranslateElementInit;
@@ -59,7 +123,7 @@ const TranslateComponent: React.FC = () => {
     }, []);
 
     return (
-        <div ref={googleTranslateRef} className="flex justify-center items-end ">
+        <div ref={googleTranslateRef} className="flex justify-center items-end">
             {/* Google Translate will render here */}
         </div>
     );
