923f42cf55a101e2d68e8f7cdd1b9d04b9ad88d0
devmahin <mahinhowlader123@gmail.com>
2024-10-22 21:03:37 +0530

    manage user role admin funtionality completed

--- src/components/Dashboard/User/UserLisItem/UserLisItem.tsx
+++ src/components/Dashboard/User/UserLisItem/UserLisItem.tsx
@@ -1,8 +1,62 @@
+import Swal from "sweetalert2";
 import TableTd from "../../../shared/TableTd";
 import { MdBlock } from "react-icons/md";
+import { useMutation } from "@tanstack/react-query";
+import useAxiosPublic from "../../../../Hooks/useAxiosPublic";
 
-const UserLisItem = ({ userData }: { userData: any }) => {
-  const {name,email,profileImage} = userData;
+type UserResponse = {
+  id: string;
+  email: string;
+  role: string
+  // Add any other properties your API response returns for a user
+}[];
+
+const UserLisItem = ({ userData,refetch }: { userData: any,refetch : any }) => {
+
+  const { name, email, profileImage, role } = userData;
+  const axiosPublic = useAxiosPublic()
+
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async (email : {email : string}) => {
+      const res = await axiosPublic.patch(`/manage-users/${email}`);
+      return res.data;
+    },
+    onSuccess: (data) => {
+      Swal.fire({
+        title: "Success!",
+        text: "Role has been changed.",
+        icon: "success"
+      });
+    refetch()
+    },
+    onError: (err) => {
+      console.log(err.message);
+    },
+  });
+
+  
+    const sweetAlert =  (email : {email : string}) => {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "Do you want to make this user an admin?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes!"
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          await mutateAsync(email)// Trigger the mutation with the user's email
+        
+        }
+      });
+    };
+  
+
+
+
+
   return (
     <tr className="odd:bg-gray-50">
       <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
@@ -17,7 +71,19 @@ const UserLisItem = ({ userData }: { userData: any }) => {
 
       <td className="whitespace-nowrap px-4 py-2 text-base font-medium text-gray-800">
         <div className="flex gap-1 justify-center">
-          <button className="btn-primary"><MdBlock /> Block User</button>
+          {role === "user" && (
+            <button
+              onClick={() => sweetAlert(userData?.email)} // Pass the email directly
+              className="btn-primary"
+            >
+              <MdBlock /> User
+            </button>
+          )}
+          {role === "admin" && (
+            <button className="btn-admin">
+              <MdBlock /> Admin
+            </button>
+          )}
         </div>
       </td>
     </tr>
