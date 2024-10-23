c51c66edc47e60cc2efee972a5608c686f973274
hammadsadi <hammad.sadi@yahoo.com>
2024-10-23 22:36:31 +0600

    product image showed on the details modal

--- src/components/Dashboard/ManageOrders/SinglerOrderAndUserDetails/SinglerOrderAndUserDetails.tsx
+++ src/components/Dashboard/ManageOrders/SinglerOrderAndUserDetails/SinglerOrderAndUserDetails.tsx
@@ -5,6 +5,10 @@ const SinglerOrderAndUserDetails = ({order}:OrderTypesProps) => {
   return (
     <div>
         {/* Product Image */}
+        <div className="flex justify-center items-center gap-2 flex-wrap">
+            {order?.product_image?.map((ig:string, idx:number) => <div key={idx}><img src={ig} className="max-w-20" alt="" /></div>)}
+            
+        </div>
     </div>
   )
 }
